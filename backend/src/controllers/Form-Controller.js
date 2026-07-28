ca21c87a746662ace33ee7a86e6ae669fff1d10b
deepak <deepakvishwakarmajs@gmail.com>
2026-07-28 20:17:52 +0530

    bugs occured

--- backend/src/controllers/Form-Controller.js
+++ backend/src/controllers/Form-Controller.js
@@ -15,10 +15,38 @@ let CreateForm=asyncHandler(
         //we get user id in request always as we said it to during logined creation time
         //with the help of jwt once auth middleware get processed it will get it to us with the help of request inside
         //the below object is available inside in request object as we set in auth middleware phase after retriving it from request 
-        let userIDinrequest=req.userPayloadData
-        let mongooseFormUserId=new mongoose.Types.ObjectId(userIDinrequest)
+      
+        // let userIDinrequest=req.userPayloadData
+        
         //extracting body data which comes in request
-        let {formName}=req.body
+        let {formName,userId}=req.body
+        //it giving me bugs so i omit this line 
+        //the bug is like when using object id in this way the document can,t matches and 
+        //not returning in lookups 
+        //the structure is looks like 
+        /*
+        _id:{
+        $oid:"random id"
+        }
+        userId:{
+        $oid:"aboveid"
+        }
+
+        if using like above it on lookup if we do aggregatin like this way
+        $lookup{
+           see forms 
+           _id
+           userId
+        }
+           we get write answer and mathcin doc but the user id is store that time directly
+            like this way not like above mentioned way in line no 31
+            userId:"randomId"
+
+            if we compare above look up with .oid we get unexpected results like the unmathc document we
+            get find out its reason then use mongoose object id types otherwise not
+        */
+        // let mongooseFormUserId=new mongoose.Types.ObjectId(userId)
+        // console.log("the mongoose id is now",mongooseFormUserId)
         
         let randomFormId=nanoid(12)
 
@@ -36,7 +64,7 @@ let CreateForm=asyncHandler(
         //we already resolve promise as save gives save doc back after resolving promise
         let savedDocument=await forms.create({
               formname:formName,
-              userId:mongooseFormUserId,
+              userId:userId,
               formId:randomFormId
         })
 
