0a7af468b7a1d5f17529f6c5a5a7810cd295bdcd
deepak <deepakvishwakarmajs@gmail.com>
2026-08-02 19:37:20 +0530

    VALIDATORS ADD AND TEST

--- backend/src/validators/Form-validator.js
+++ backend/src/validators/Form-validator.js
@@ -1,34 +1,29 @@
 import joi from "joi"
 import { parsePhoneNumberFromString } from "libphonenumber-js";
-import { postcodeValidator } from "postcode-validator";
-// import countries from "i18n-iso-countries";
+import {verifyPhoneNumberStructure,Alpha2ISOCountryCode} from "../utils/constant.js";
 
-// import en from "i18n-iso-countries/langs/en.json" assert { type: "json" };
-
-// countries.registerLocale(en);
-
-// let alpha2CodesObject=countries.getAlpha2Codes()
-
-// let alpha2CodesCountry=Object.keys(alpha2CodesObject)
 
 function createDyanmicSchemaValidation(requestBody){
 
     console.log("inside dynamic validationfuncoitn here 😀");
-    
+ 
     //basic schema rule object
     let schemaRules=  {
         "ff-sender-firstName":joi.string().max(30),
         "ff-sender-email":joi.string().email(),
         "ff-sender-lastName":joi.string().max(30),
         "ff-sender-fullName":joi.string().max(50),
-        "ff-sender-phone":joi.string().pattern(/^\+[1-9]\d{1,14}$/,"E164format"),
+        "ff-sender-phone":joi.string().pattern(/^\+[1-9]\d{1,14}$/,"E164format").custom(verifyPhoneNumberStructure),
         "ff-sender-title":joi.string().max(10).optional(),
         "ff-sender-company":joi.string().max(15).optional(),
         "ff-sender-address":joi.string().max(60).optional(),
         "ff-sender-address2":joi.string().max(60).optional(),
+        //i just added here post code we can,t verify whether it is actuly post code or not
         "ff-sender-city":joi.string().min(1).max(200),
-        "ff-sender-postcode":joi.string().min(3).max(20)
-        // "ff-sender-country":joi.string().valid(...alpha2CodesCountry)
+        "ff-sender-postcode":joi.string().min(3).max(20),
+        //HERE IS SOME KIND OF BUG IN THIS LINE WHY IT NOT STOPPING AT MAX LIMIT HIT
+        "ff-sender-country":joi.string().max(2).valid(...Alpha2ISOCountryCode).messages({"any.only":"INVALID ISO ALPHA2 COUNTRY CODE"}),
+       "ff-sender-dob":joi.date().iso().less(new Date().toISOString()).messages({"date.less":`DATE OF BIRTH  SHOULD BE LESS THAN EQUAL TO  ${new Date().toLocaleString()}`})
     }
   
   //regex pattern for valid structure like ff-blocktype-propertyname
@@ -77,6 +72,19 @@ function createDyanmicSchemaValidation(requestBody){
         if(fieldName.includes('rating'))
         {
         schemaRules[fieldName]=joi.number().min(1).max(5)
+        }
+        if(fieldName.includes('country'))
+        {
+        schemaRules[fieldName]=joi.string().max(2).message("{{#label}} iso alpha2 code should be max 2 character").valid(...Alpha2ISOCountryCode)
+        }
+        if(fieldName.includes('date'))
+        {
+            // 2060 year 1 januar first din se kam wali sare date allowed hai uske upar wali nhi allowed hai
+        schemaRules[fieldName]=joi.date().iso().less("2060-01-01").messages(
+            {
+                "date.less":"CAN NOT ALLOWED TO ADD DATE GREATER THAN 2060-01-01"
+            }
+        )
         }
     }
 )
