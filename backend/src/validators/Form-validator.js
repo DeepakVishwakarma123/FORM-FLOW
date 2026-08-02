78ec569c4d503c7fa57e15ce51236e7020819606
deepak <deepakvishwakarmajs@gmail.com>
2026-08-02 16:26:11 +0530

    form validator add

--- backend/src/validators/Form-validator.js
+++ backend/src/validators/Form-validator.js
@@ -11,10 +11,12 @@ import { postcodeValidator } from "postcode-validator";
 
 // let alpha2CodesCountry=Object.keys(alpha2CodesObject)
 
+function createDyanmicSchemaValidation(requestBody){
 
-
-let formSchema=joi.object(
-    {
+    console.log("inside dynamic validationfuncoitn here 😀");
+    
+    //basic schema rule object
+    let schemaRules=  {
         "ff-sender-firstName":joi.string().max(30),
         "ff-sender-email":joi.string().email(),
         "ff-sender-lastName":joi.string().max(30),
@@ -28,16 +30,76 @@ let formSchema=joi.object(
         "ff-sender-postcode":joi.string().min(3).max(20)
         // "ff-sender-country":joi.string().valid(...alpha2CodesCountry)
     }
+  
+  //regex pattern for valid structure like ff-blocktype-propertyname
+  let pattern=/^[f]{2}-\b(text|rating|select|date|country|url|number|phone|email)-[a-z-A-Z]{2,20}$/
+ 
+  //converting requestbody keys into array
+  //i think i have to log requestbody before procedding it
+  //let see what happen when we run actual code
+  console.log("the request body is now",requestBody)
+  let requestFields=Object.keys(requestBody)
+
+  console.log(requestFields)
+   //filterign appropritate field who passed the regex pattern
+   let validFieldNames=requestFields.filter(
+            (fieldNames) => {
+                return pattern.test(fieldNames)
+            }
+        )
+        console.log("valid field names are",validFieldNames);
+
+
+    
+    //adding custom joi validatoin code for appropriate fields
+    validFieldNames.forEach(
+        
+        (fieldName) => {
+        console.log("hello hii ho ware you");
+        if(fieldName.includes('url'))
+        {
+        //addign custom error message here to avid confusion of uri
+        schemaRules[fieldName]=joi.string().uri().message("{{#label}} must be valid url")
+        }
+        if(fieldName.includes('text'))
+        {
+        //NOT GIVING MINIMUM VALUE  FOR WRITE NOW
+        schemaRules[fieldName]=joi.string().max(200)
+        }
+        if(fieldName.includes('number'))
+        {
+        schemaRules[fieldName]=joi.number()
+        }
+        if(fieldName.includes('email'))
+        {
+        schemaRules[fieldName]=joi.string().email()
+        }
+        if(fieldName.includes('rating'))
+        {
+        schemaRules[fieldName]=joi.number().min(1).max(5)
+        }
+    }
+)
+
+let formSchema=joi.object(
+  schemaRules
 )
 
 
 
+return formSchema
+}
+
+/*this will not write approach but let'see its pros and cons
+let see how much time it take to server bascilly
+*/
+
 function validate(req,res,next) {
     console.log("hello inside validate function");
     
+    let requestBody=req.body
+    let formSchema=createDyanmicSchemaValidation(requestBody)
     let {error,value}=formSchema.validate(req.body)  
-    console.log(error);
-    console.log(value);
    if(error===undefined)
     {
         next()
@@ -45,7 +107,7 @@ function validate(req,res,next) {
     }
   res.status(200).json(
     {
-        error
+     "error details":error["details"][0]
     }
   )
 }
@@ -54,3 +116,4 @@ function validate(req,res,next) {
 export default validate
 
 // ^[+]([1-9]{2}[1-9]?[0-9]*){3,15}
+
