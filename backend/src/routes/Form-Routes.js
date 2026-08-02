c5bbb7409855c06ddd0475db137a1eabe05930cc
deepak <deepakvishwakarmajs@gmail.com>
2026-08-02 16:26:38 +0530

    modifying route

--- backend/src/routes/Form-Routes.js
+++ backend/src/routes/Form-Routes.js
@@ -1,10 +1,13 @@
 import Router from "express"
 import { CreateForm } from "../controllers/Form-Controller.js"
+import { RecordSubmission } from "../controllers/Form-Controller.js"
+import validate from "../validators/Form-validator.js"
 
 const formRouter=Router()
 
 
 formRouter.route("/createform").post(CreateForm)
+formRouter.route("/submit").post(validate,RecordSubmission)
 
 
 
