30e38faa206479bcb726cb30ecbdd1fbc31547d8
deepak <deepakvishwakarmajs@gmail.com>
2026-08-03 23:56:15 +0530

    sender property added in model

--- backend/src/models/submission.model.js
+++ backend/src/models/submission.model.js
@@ -6,11 +6,21 @@ const formsubmissionSchema=new Schema(
     {
         formid:{
             type:Schema.Types.ObjectId,
-            ref:"forms"
+            ref:"forms",
+            required:[true,"formId Required"]
         }
     }
 )
 
+
+//to modify schema after initian later use .add method on schemaObject and insert the object with appropriate fields and supoprt for it
+formsubmissionSchema.add(
+    {
+        sender:{
+            type:Object
+        }
+    }
+)
 
 const formSubmissions=new mongoose.model("formsubmission",formsubmissionSchema)
 
