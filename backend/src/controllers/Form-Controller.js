8404953f68a0e94d5d75b81fe1e3c3adc109c019
deepak <deepakvishwakarmajs@gmail.com>
2026-08-03 23:55:49 +0530

    record data to db fixed structure not dyanmic fields

--- backend/src/controllers/Form-Controller.js
+++ backend/src/controllers/Form-Controller.js
@@ -54,14 +54,58 @@ let RecordSubmission=asyncHandler(
     async function (req,res) {
         let {formId}=req.query
 
-        //body from data 
+        //the controller reac here only if validators are passed out otherwise not
+        //so here we have valid values as object
+
+        //hamare pass data kuch aisa hai 
+        /*
+        {
+        ff-sender-firstName:"rahul riddhi",
+        ff-url-kartik:"https://",
+        ff-sender-dob:"2444"
+        }
+        */
         let userData=req.body
+        let fieldKeysArray=Object.keys(userData)
+
+        //ab hum valid keys ko valid db main valid fields main insert karenge uske liye hum
+        //sabse phele keys ka array lenge  jo specfic fiedl hai unhe unko appropritate field main daal denge bus itna hi hai 
+        
+        //yeh sirf sender wali keys pure array main se dega bus
+        let fieldWithSenderAttribute=fieldKeysArray.filter(
+            (fieldKeyName) => {
+                return fieldKeyName.includes('sender')
+            }
+        )
+        
+        let submissionDoc=new formSubmissions()
+
+        //abhi humne aise le liya hai karke per kaunsi field ko kis data ke ander db maind aale yeh hume pata nhi hai 
+        //yaa koi fix order nhi ho sakta hai udhar toh jo valid fiedls hai unhe hume unke hissab se valid schema main sender doc ke keys main dalana hoga jasie jo name hai usme firstName wale field aaye karke
+        
+        let documentTobePut={}
+        fieldKeysArray.forEach(
+            (keyName) => {
+                //yeh code data ko full uske form main dalega like 
+                //ff-url-siteName kuch is tarah se
+
+                //dusra tariak hi hai usme hume speic name pe field dalan chhate hai per woh
+                //hume bhoht cheks karne pdenge
+                documentTobePut[keyName]=userData[keyName]
+            }
+        )
+        submissionDoc.sender=documentTobePut
+
+   let savedDoc=await submissionDoc.save()
 
         console.log(userData)
+
+        //firs of all let insert value to specfic key in db by filterting them
+
         res.status(200).json(
             {
                 "message":"everythign is okk here",
-                "userdata":userData
+                "userdata":savedDoc
             }
         )
     }
