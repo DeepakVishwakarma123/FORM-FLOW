9b8ba66bac0259fb9423a1030ad875a0d8a405c0
deepak <deepakvishwakarmajs@gmail.com>
2026-08-14 08:41:31 +0530

    model modify

---
+++ backend/src/models/customFields.model.js
@@ -0,0 +1,23 @@
+import mongoose, { mongo } from "mongoose";
+import { Schema } from "mongoose";
+import { validBlockTypes } from "../utils/constant.js";
+
+let cutomFieldSchema=mongoose.Schema(
+    {
+        fieldName:{
+            type:String
+        },
+        blockType:{
+            type:String,
+            enum:validBlockTypes
+        },
+        formid:{
+            type:Schema.Types.ObjectId,
+            ref:"forms"
+        }
+    }
+)
+
+let customFields=mongoose.model(cutomFieldSchema)
+
+export default customFields
