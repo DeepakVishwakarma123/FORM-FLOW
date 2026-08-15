6f8369d513fdaf76c3e60ce99ceb985390e60fe7
deepak <deepakvishwakarmajs@gmail.com>
2026-08-15 16:59:28 +0530

    formidchekc middleware function add

--- backend/src/routes/Form-Routes.js
+++ backend/src/routes/Form-Routes.js
@@ -2,12 +2,13 @@ import Router from "express"
 import { CreateForm } from "../controllers/Form-Controller.js"
 import { RecordSubmission } from "../controllers/Form-Controller.js"
 import validate from "../validators/Form-validator.js"
+import { formIdMiddleware } from "../middleware/formIdCheckMiddleware.js"
 
 const formRouter=Router()
 
 
 formRouter.route("/createform").post(CreateForm)
-formRouter.route("/submit/:formid").post(validate,RecordSubmission)
+formRouter.route("/submit/:formid").post(formIdMiddleware,validate,RecordSubmission)
 
 
 
