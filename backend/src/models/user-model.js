67d7ecea9428e4869fa54ed6d4829f8503388923
deepak <deepakvishwakarmajs@gmail.com>
2026-08-20 13:22:58 +0530

    pre hook add ,custom method jwt token generation add

--- backend/src/models/user-model.js
+++ backend/src/models/user-model.js
@@ -1,23 +1,26 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
+import bcrypt from "bcryptjs"
+import jsonwebtoken  from "jsonwebtoken"
 
 
 const userSchema=new Schema(
     {
-        username:{
-            type:String,
-            required:true,
-        },
         refreshToken:{
             type:String,
-            required:true
+            default:null
         },
         forgotPasswordToken:{
-            type:String
+            type:String,
+            default:null
         },
         hashedPassword:{
             type:String,
             required:true
+        },
+        verficationToken:{
+            type:String,
+            default:null
         },
         emailId:{
             type:String,
@@ -29,6 +32,49 @@ const userSchema=new Schema(
         }
     }
 )
+
+//let add password hash hook in mongoose schema
+userSchema.pre(
+    'save',async function () {
+        console.log("the this is something here bro code",this)
+        let unhashedPassword=this.hashedPassword
+        console.log(unhashedPassword);
+        
+        let hashedPassword=await bcrypt.hash(unhashedPassword,10)
+        this.hashedPassword=hashedPassword
+    }
+)
+
+userSchema.methods.verifyPassword=async function (enteredPassword) {
+  let verificationResult=await bcrypt.compare(enteredPassword,this.hashedPassword)
+  return verificationResult
+}
+
+userSchema.methods.generateAccessToken=function () {
+    let userId=this._id
+    let accessToken=jsonwebtoken.sign(
+        {userId},
+        process.env.acc_secret,
+        {
+            algorithm:"HS256",
+            expiresIn:"2m"
+        }
+    )
+    return accessToken
+}
+
+userSchema.methods.generateRefreshToken=function () {
+    let userId=this._id
+    let refreshToken=jsonwebtoken.sign(
+        {"ekbewafahai":"ekbewafahai"},
+        process.env.rcc_secret,
+        {
+            algorithm:"HS256",
+            expiresIn:"2d"
+        }
+    )
+    return refreshToken
+}
 
 const users=new mongoose.model("user",userSchema)
 
