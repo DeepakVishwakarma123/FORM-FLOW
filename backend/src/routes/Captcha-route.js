9fd0eb2c7050d88575d0bb5c338b643cc435c643
deepak <deepakvishwakarmajs@gmail.com>
2026-08-30 20:52:02 +0530

    new route activate captcha add

--- backend/src/routes/Captcha-route.js
+++ backend/src/routes/Captcha-route.js
@@ -1,10 +1,11 @@
-import { Addcaptcha } from "../controllers/captcha-controller.js";
+import { activateSpeficCapthca, Addcaptcha } from "../controllers/captcha-controller.js";
 import {Router} from 'express'
 
 const captchaRouter=Router()
 
 captchaRouter.route("/addsecretkey").post(Addcaptcha)
 
+captchaRouter.route("/activate").post(activateSpeficCapthca)
 // captchaRouter.route("/activate").post(Addcaptcha)
 
 export {captchaRouter}
