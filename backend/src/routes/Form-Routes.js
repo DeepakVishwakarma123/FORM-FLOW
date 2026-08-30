43704227b5060e393cfec1d27a7888235d5ab2fb
deepak <deepakvishwakarmajs@gmail.com>
2026-08-30 20:51:27 +0530

    capthcha chek middleware add

--- backend/src/routes/Form-Routes.js
+++ backend/src/routes/Form-Routes.js
@@ -3,13 +3,13 @@ import { CreateForm } from "../controllers/Form-Controller.js"
 import { RecordSubmission } from "../controllers/Form-Controller.js"
 import validate from "../validators/Form-validator.js"
 import { formIdMiddleware } from "../middleware/formIdCheckMiddleware.js"
-
+import captchaCheckmiddleware from "../middleware/captchaCheckmiddleware.js"
 
 const formRouter=Router()
 
 
 formRouter.route("/createform").post(CreateForm)
-formRouter.route("/submit/:formid").post(formIdMiddleware,validate,RecordSubmission)
+formRouter.route("/submit/:formid").post(formIdMiddleware,captchaCheckmiddleware,validate,RecordSubmission)
 
 
 
