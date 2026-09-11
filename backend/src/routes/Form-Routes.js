fab02b325f7792e9d457d2ee4104e6ae04175aa0
deepak <deepakvishwakarmajs@gmail.com>
2026-09-11 10:05:18 +0530

    form route update

--- backend/src/routes/Form-Routes.js
+++ backend/src/routes/Form-Routes.js
@@ -4,12 +4,12 @@ import { RecordSubmission } from "../controllers/Form-Controller.js"
 import validate from "../validators/Form-validator.js"
 import { formIdMiddleware } from "../middleware/formIdCheckMiddleware.js"
 import captchaCheckmiddleware from "../middleware/captchaCheckmiddleware.js"
-
+import { AuthorizeDomainMiddleware } from "../middleware/AuthorizeDomain.middleware.js"
 const formRouter=Router()
 
 
 formRouter.route("/createform").post(CreateForm)
-formRouter.route("/submit/:formid").post(formIdMiddleware,captchaCheckmiddleware,validate,RecordSubmission)
+formRouter.route("/submit/:formid").post(AuthorizeDomainMiddleware,captchaCheckmiddleware,validate,RecordSubmission)
 
 
 
